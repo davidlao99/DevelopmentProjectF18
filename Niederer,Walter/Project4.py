959111e3f9728b547d953e44aab8722f51336fd4
Walt <wpniederer@cpp.edu>
2018-10-11 11:31:40 -0700

    debugging merge_files function

--- Niederer,Walter/Project4.py
+++ Niederer,Walter/Project4.py
@@ -6,7 +6,7 @@ import itertools
 # 400, 32kb, sorted up to 500 lines
 # 500, same as aboce
 
-NUM_TO_READ = 500
+NUM_TO_READ = 425
 ARRAY_SIZE = 8500
 
 
@@ -62,6 +62,46 @@ def merge(arr, l, m, r):
         k += 1
 
 
+def merge_arrays(arr1, arr2, n1, n2):
+    arr3 = [None] * (n1 + n2)
+    i = 0
+    j = 0
+    k = 0
+
+    # Traverse both array
+    while i < n1 and j < n2:
+
+        # Check if current element
+        # of first array is smaller
+        # than current element of
+        # second array. If yes,
+        # store first array element
+        # and increment first array
+        # index. Otherwise do same
+        # with second array
+        if arr1[i] < arr2[j]:
+            arr3[k] = arr1[i]
+            k = k + 1
+            i = i + 1
+        else:
+            arr3[k] = arr2[j]
+            k = k + 1
+            j = j + 1
+
+    # Store remaining elements
+    # of first array
+    while i < n1:
+        arr3[k] = arr1[i];
+        k = k + 1
+        i = i + 1
+
+    # Store remaining elements
+    # of second array
+    while j < n2:
+        arr3[k] = arr2[j]
+        k = k + 1
+        j = j + 1
+
 def read_ints(filename, start, end):
     divided_list = []
     with open(filename) as int_file:
@@ -87,7 +127,49 @@ def write_ints_append(filename, list_to_write):
 
 
 def merge_files(files):
-    print("hello")
+    # iterate through all the files
+    buffer = []
+    total_nums = 0
+    nums_total = 0
+    for i in range(0, len(files)):
+        for file in files:
+            for num in read_ints(file, i * 25, (i + 1) * 25):
+                print(i)
+                if len(buffer) < 500:
+                    buffer.append(num)
+                    total_nums += 1
+
+                # i = 1 by this point...probably why 500 elems are dropped
+                else:
+                    merge_sort(buffer, 0, len(buffer) - 1)
+                    print(len(buffer))
+                    file_biggest_int = read_ints("output.txt", (i) * 499, (i) * 500)
+
+                    print(len(file_biggest_int))
+                    if len(file_biggest_int) == 0:
+                        write_ints_append("output.txt", buffer)
+                        buffer.clear()
+                    print(file_biggest_int)
+
+                    if buffer[0] > file_biggest_int[0]:
+                        write_ints_append("output.txt", buffer)
+                        buffer.clear()
+
+                    else:
+                        print(i)
+                        array_2 = read_ints("output.txt", (i) * 500, (i + 1) * 500)
+                        print(array_2)
+                        print(len(array_2))
+                        print(len(buffer))
+                        merge_arrays(buffer, array_2, len(buffer), 500)
+                        buffer.clear()
+
+        # print("Length of buffer: " + str(len(buffer)))
+
+    print("Total ints processed in else: " + str(nums_total))
+    print("Total ints processed in if: " + str(total_nums))
+    is_sorted("output.txt")
+
 
 def is_sorted(filename):
     sorted_array = []
@@ -118,22 +200,23 @@ for i in range(0, ARRAY_SIZE // NUM_TO_READ):
     # print("Iteration: " + str(i))
 
 
-divided_file = []
-buffer_file = []
-total_nums = 0
-num_to_read_from_file = NUM_TO_READ // len(file_list)
-for i in range(0, len(file_list) + 1):
-    for file in file_list:
-        for x in read_ints(file, i * num_to_read_from_file, (i + 1) * num_to_read_from_file):
-            if len(buffer_file) <= 500:
-                buffer_file.append(x)
-            else:
-                merge_sort(buffer_file, 0, len(buffer_file) - 1)
-                write_ints_append("output.txt", buffer_file)
-                buffer_file.clear()
+# divided_file = []
+# buffer_file = []
+# total_nums = 0
+# num_to_read_from_file = NUM_TO_READ // len(file_list)
+# for i in range(0, len(file_list) + 1):
+#     for file in file_list:
+#         for x in read_ints(file, i * num_to_read_from_file, (i + 1) * num_to_read_from_file):
+#             if len(buffer_file) <= 500:
+#                 buffer_file.append(x)
+#             else:
+#                 merge_sort(buffer_file, 0, len(buffer_file) - 1)
+#                 write_ints_append("output.txt", buffer_file)
+#                 buffer_file.clear()
+#
+#         print(len(buffer_file))
 
-        print(len(buffer_file))
-
+merge_files(file_list)
 is_sorted("output.txt")
 
         # if len(buffer_file) >= 500:
