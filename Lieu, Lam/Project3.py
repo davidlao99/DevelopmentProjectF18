75df883ddcc565205bcd04ae9eb4fe6c697db682
LamLieu <LamLieu@users.noreply.github.com>
2018-10-11 00:58:44 -0700

    Created schedule and group member for myself

--- Lieu, Lam/Project3.py
+++ Lieu, Lam/Project3.py
@@ -1,16 +1,21 @@
+
 class Group2:
     def meet_up(self, day, time):
-        self.day = day
-        self.time = time
+
     def print_members(self):
 
 class GroupMember:
-    def __init__(self, name, major, classSchdule):
+    def __init__(self, name, major, classSchedule):
         self.name = name
         self.major = major
-        self.classSchedule = classSchdule
+        self.classSchedule = classSchedule
     def print_schedule(self):
-        for
+        print("Name: " + str(self.name) + "\nMajor: " + str(self.major))
+        print("*** Class Schedule ***")
+
+        for i in range(len(self.classSchedule)):
+            print(self.classSchedule[i].print_class_info)
+
 class Class:
     def __init__(self, department, classNumber, startTime, endTime, meetingDays):
         self.department = department
@@ -26,3 +31,16 @@ class Class:
         print('End Time: ' + self.endTime)
         for days in enumerate(meetingDays):
             print(days)
+
+def lam_schedule():
+    schedule = Class("CS", "2400", 10.45, 11.50, "M/W/F")
+    return schedule
+
+def group_member_lam():
+    Lam = GroupMember("Lam Lieu", "Computer Science", lam_schedule())
+    return Lam
+
+def all_group_members():
+    groupMembers = [group_member_lam()]
+    return groupMembers
+
