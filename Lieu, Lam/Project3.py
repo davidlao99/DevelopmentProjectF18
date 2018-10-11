34554092acb8f03bbab20c0af97b00505f7dde5b
LamLieu <LamLieu@users.noreply.github.com>
2018-10-11 00:31:19 -0700

    Small update

--- Lieu, Lam/Project3.py
+++ Lieu, Lam/Project3.py
@@ -1,24 +1,28 @@
-class Group_2():
-    groupMembers = ['Lam Lieu', 'Neha Jayan', 'Marlon Aquino', 'David Lao'];
+class Group2:
     def meet_up(self, day, time):
-        print('hello')
+        self.day = day
+        self.time = time
     def print_members(self):
 
-class GroupMember():
+class GroupMember:
     def __init__(self, name, major, classSchdule):
         self.name = name
         self.major = major
         self.classSchedule = classSchdule
-
-class Class():
-    def __init__(self, department, classNumber, startTime, endTime):
+    def print_schedule(self):
+        for
+class Class:
+    def __init__(self, department, classNumber, startTime, endTime, meetingDays):
         self.department = department
         self.classNumber = classNumber
         self.startTime = startTime
         self.endTime = endTime
+        self.meetingDays = meetingDays
 
     def print_class_info(self):
         print('Department: ' + self.department)
         print('Class Number: ' + self.classNumber)
         print('Start Time: ' + self.startTime)
         print('End Time: ' + self.endTime)
+        for days in enumerate(meetingDays):
+            print(days)
